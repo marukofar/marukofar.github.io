5c4ba3afef85b7bf03bac84fe3833285a715f3bc
Fareed Marnleb <far1.conan@gmail.com>
2020-11-26 19:02:17 +0700

    loop

--- Love_in_the_loop.py
+++
@@ -1,6 +0,0 @@
-loop = 10
-
-for i in range(loop, 0, -1):
-    for k in range(0, i):
-        print("love", end=' ')
-    print("\n")
---
+++ loop.py
@@ -0,0 +1,4 @@
+loop = 3
+
+for i in range(loop):
+    print("love")
